88a28d9042473179d81fc4eb2619fa1a5d7b008f
Wilson Alberto <wilsonalberto@gmail.com>
2019-01-28 11:11:25 +0000

    chore: Refresh gitignore and minor updates

--- src/sdk/awardList.js
+++ src/sdk/awardList.js
@@ -68,111 +68,220 @@ const awardList = [
       },
     ],
   },
-//   {
-//     id: 3,
-//     title: 'Actress in a Supporting Role',
-//     nominees: [],
-//   },
-//   {
-//     id: 4,
-//     title: 'Actor in a Supporting Role',
-//     nominees: [],
-//   },
-//   {
-//     id: 5,
-//     title: 'Directing',
-//     nominees: [],
-//   },
-//   {
-//     id: 6,
-//     title: 'Adapted Screenplay',
-//     nominees: [],
-//   },
-//   {
-//     id: 7,
-//     title: 'Original Screenplay',
-//     nominees: [],
-//   },
-//   {
-//     id: 8,
-//     title: 'Foreign Language Film',
-//     nominees: [],
-//   },
-//   {
-//     id: 9,
-//     title: 'Animated Feature',
-//     nominees: [],
-//   },
-//   {
-//     id: 10,
-//     title: 'Original Score',
-//     nominees: [],
-//   },
-//   {
-//     id: 11,
-//     title: 'Original Song',
-//     nominees: [],
-//   },
-//   {
-//     id: 12,
-//     title: 'Documentary Short',
-//     nominees: [],
-//   },
-//   {
-//     id: 13,
-//     title: 'Cinematography',
-//     nominees: [],
-//   },
-//   {
-//     id: 14,
-//     title: 'Best Documentary Feature',
-//     nominees: [],
-//   },
-//   {
-//     id: 15,
-//     title: 'Production Design',
-//     nominees: [],
-//   },
-//   {
-//     id: 16,
-//     title: 'Sound Mixing',
-//     nominees: [],
-//   },
-//   {
-//     id: 17,
-//     title: 'Costume Design',
-//     nominees: [],
-//   },
-//   {
-//     id: 18,
-//     title: 'Film Editing',
-//     nominees: [],
-//   },
-//   {
-//     id: 19,
-//     title: 'Sound Editing',
-//     nominees: [],
-//   },
-//   {
-//     id: 20,
-//     title: 'Animated Short Film',
-//     nominees: [],
-//   },
-//   {
-//     id: 21,
-//     title: 'Live Action Short',
-//     nominees: [],
-//   },
-//   {
-//     id: 22,
-//     title: 'Makeup and Hairstyle',
-//     nominees: [],
-//   },
-//   {
-//     id: 23,
-//     title: 'Visual Effects',
-//     nominees: [],
-//   },
+  {
+    id: 3,
+    title: 'Actress in a Leading Role',
+    nominees: [
+      {
+        id: 1775557,
+        type: 'people',
+        movie: 426426,
+      },
+      {
+        id: 515,
+        type: 'people',
+        movie: 340613,
+      },
+      {
+        id: 39187,
+        type: 'people',
+        movie: 375262,
+      },
+      {
+        id: 237405,
+        type: 'people',
+        movie: 332562,
+      },
+      {
+        id: 55536,
+        type: 'people',
+        movie: 401847,
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: 'Actress in a Supporting Role',
+    nominees: [
+      {
+        id: 9273,
+        type: 'person',
+        movie: 429197,
+      },
+      {
+        id: 78883,
+        type: 'person',
+        movie: 426426,
+      },
+      {
+        id: 9788,
+        type: 'person',
+        movie: 465914,
+      },
+      {
+        id: 54693,
+        type: 'person',
+        movie: 375262,
+      },
+      {
+        id: 3293,
+        type: 'person',
+        movie: 375262,
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: 'Actor in a Supporting Role',
+    nominees: [
+      {
+        id: 932967,
+        type: 'person',
+        movie: 490132,
+      },
+      {
+        id: 1023139,
+        type: 'person',
+        movie: 487558,
+      },
+      {
+        id: 16431,
+        type: 'person',
+        movie: 332562,
+      },
+      {
+        id: 20766,
+        type: 'person',
+        movie: 401847,
+      },
+      {
+        id: 6807,
+        type: 'person',
+        movie: 429197,
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: 'Directing',
+    nominees: [
+      {
+        id: 487558,
+        type: 'person',
+        movie: 5281,
+      },
+      {
+        id: 440298,
+        type: 'person',
+        movie: 64194,
+      },
+      {
+        id: 375262,
+        type: 'person',
+        movie: 122423,
+      },
+      {
+        id: 426426,
+        type: 'person',
+        movie: 11218,
+      },
+      {
+        id: 429197,
+        type: 'person',
+        movie: 55710,
+      },
+    ],
+  },
+  // {
+  //   id: 7,
+  //   title: 'Adapted Screenplay',
+  //   nominees: [],
+  // },
+  // {
+  //   id: 8,
+  //   title: 'Original Screenplay',
+  //   nominees: [],
+  // },
+  // {
+  //   id: 9,
+  //   title: 'Foreign Language Film',
+  //   nominees: [],
+  // },
+  // {
+  //   id: 10,
+  //   title: 'Animated Feature',
+  //   nominees: [],
+  // },
+  // {
+  //   id: 11,
+  //   title: 'Original Score',
+  //   nominees: [],
+  // },
+  // {
+  //   id: 12,
+  //   title: 'Original Song',
+  //   nominees: [],
+  // },
+  // {
+  //   id: 13,
+  //   title: 'Documentary Short',
+  //   nominees: [],
+  // },
+  // {
+  //   id: 14,
+  //   title: 'Cinematography',
+  //   nominees: [],
+  // },
+  // {
+  //   id: 15,
+  //   title: 'Best Documentary Feature',
+  //   nominees: [],
+  // },
+  // {
+  //   id: 16,
+  //   title: 'Production Design',
+  //   nominees: [],
+  // },
+  // {
+  //   id: 17,
+  //   title: 'Sound Mixing',
+  //   nominees: [],
+  // },
+  // {
+  //   id: 18,
+  //   title: 'Costume Design',
+  //   nominees: [],
+  // },
+  // {
+  //   id: 19,
+  //   title: 'Film Editing',
+  //   nominees: [],
+  // },
+  // {
+  //   id: 20,
+  //   title: 'Sound Editing',
+  //   nominees: [],
+  // },
+  // {
+  //   id: 21,
+  //   title: 'Animated Short Film',
+  //   nominees: [],
+  // },
+  // {
+  //   id: 22,
+  //   title: 'Live Action Short',
+  //   nominees: [],
+  // },
+  // {
+  //   id: 23,
+  //   title: 'Makeup and Hairstyle',
+  //   nominees: [],
+  // },
+  // {
+  //   id: 24,
+  //   title: 'Visual Effects',
+  //   nominees: [],
+  // },
 ];
 
 export default awardList;
